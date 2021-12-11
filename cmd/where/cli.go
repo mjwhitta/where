efef1935eafe57ae55e9e86290ed538052109f57
Miles Whittaker <mj@whitta.dev>
2021-12-10 22:50:22 -0500

    Updated deps and gomk

--- cmd/where/cli.go
+++ cmd/where/cli.go
@@ -11,22 +11,21 @@ import (
 
 // Exit status
 const (
-	Good             int = 0
-	InvalidOption    int = 1
-	InvalidArgument  int = 2
-	MissingArguments int = 3
-	ExtraArguments   int = 4
-	Exception        int = 5
+	Good = iota
+	InvalidOption
+	MissingOption
+	InvalidArgument
+	MissingArgument
+	ExtraArgument
+	Exception
 )
 
 // Flags
-type cliFlags struct {
+var flags struct {
 	nocolor bool
 	verbose bool
 	version bool
 }
-
-var flags cliFlags
 
 func init() {
 	// Configure cli package
@@ -38,18 +37,16 @@ func init() {
 		[]string{
 			"Normally the exit status is 0. In the event of an error",
 			"the exit status will be one of the below:\n\n",
-			"1: Invalid option\n",
-			"2: Invalid argument\n",
-			"3: Missing arguments\n",
-			"4: Extra arguments\n",
-			"5: Exception",
+			hl.Sprintf("%d: Invalid option\n", InvalidOption),
+			hl.Sprintf("%d: Missing option\n", MissingOption),
+			hl.Sprintf("%d: Invalid argument\n", InvalidArgument),
+			hl.Sprintf("%d: Missing argument\n", MissingArgument),
+			hl.Sprintf("%d: Extra argument\n", ExtraArgument),
+			hl.Sprintf("%d: Exception", Exception),
 		},
 		" ",
 	)
-	cli.Info = strings.Join(
-		[]string{"Simple which-like example binary."},
-		" ",
-	)
+	cli.Info = "Simple which-like example binary."
 	cli.SeeAlso = []string{"command", "which"}
 	cli.Title = "Where"
 
@@ -65,7 +62,7 @@ func init() {
 		"v",
 		"verbose",
 		false,
-		"Show show stacktrace if error.",
+		"Show stacktrace, if error.",
 	)
 	cli.Flag(&flags.version, "V", "version", false, "Show version.")
 	cli.Parse()
@@ -83,8 +80,8 @@ func validate() {
 
 	// Validate cli flags
 	if cli.NArg() < 1 {
-		cli.Usage(MissingArguments)
+		cli.Usage(MissingArgument)
 	} else if cli.NArg() > 1 {
-		cli.Usage(ExtraArguments)
+		cli.Usage(ExtraArgument)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module gitlab.com/mjwhitta/where
 go 1.17
 
 require (
-	gitlab.com/mjwhitta/cli v1.8.2
-	gitlab.com/mjwhitta/hilighter v1.10.0
-	gitlab.com/mjwhitta/log v1.5.1
+	gitlab.com/mjwhitta/cli v1.9.0
+	gitlab.com/mjwhitta/hilighter v1.10.1
+	gitlab.com/mjwhitta/log v1.6.0
 	gitlab.com/mjwhitta/pathname v1.0.9
 	gitlab.com/mjwhitta/safety v1.11.0
 )
+
+require gitlab.com/mjwhitta/errors v1.0.0 // indirect
