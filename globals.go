4e31124d87097fbd0d64aa4db1c2651c7a6cfc33
Miles Whittaker <mj@whitta.dev>
2025-12-26 23:03:17 -0500

    Updated deps

--- globals.go
+++ globals.go
@@ -3,7 +3,7 @@ package where
 import "github.com/mjwhitta/safety"
 
 // Version is the package version.
-const Version string = "1.3.11"
+const Version string = "1.3.12"
 
 var (
 	cache    *safety.Map         = safety.NewMap()
